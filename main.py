1cf1b3545f8250599434150ef463b6082057b900
Admin <admin@MacBook-Pro-de-Hugo.local>
2020-10-26 16:52:57 +0100

    fonction menu contact

--- main.py
+++ main.py
@@ -1,10 +1,61 @@
-from art import *
-
-def say_hello(name):
-    print("Hello " + name)
-    print(f"How are you {name}?")
 
 
-n = "Bob"
-say_hello(n)
-print("test :")
+répertoire={
+
+}
+a = False
+
+
+def create_contact(name, nbPhone, fav2):
+
+    contact1={
+        "name": name,
+        "nbPhone": nbPhone,
+        "fav": fav2,
+    }
+    return contact1
+
+def add_contact(contact1):
+    name = contact1["name"]
+    répertoire[ name ] = contact1
+    return répertoire
+boucle1 = False
+
+def get_names(répertoire):
+    for i in répertoire:
+        print(i)
+
+
+while True :
+    a = False
+    boucle1 = False
+    print("Bienvenue dans le menu principal de ton répertoire \n Tu as le chois entre deux options \n liste des contacts \n créer un contact")
+    chois = input("tape 1 pour liste des contacts et 2 pour créer un contact")
+    if chois == "1" :
+        get_names(répertoire)
+
+
+    if chois == "2" :
+
+        #ajout contact
+        while not a:
+            while not boucle1:
+                    nbphone1 = input("écrit le numéro de téléphone?")
+                    if nbphone1.isdigit():
+                        boucle1 = True
+                        name1 = input("écrit le nom?")
+
+                    fav1 = input("Veux-tu le mettre en favory ? OUI ou NON ?")
+                    if fav1 == "OUI":
+                        fav2 = True
+                    else:
+                        fav2 = False
+
+            c = create_contact(nbphone1 , name1 , fav1 )
+            add = input("Veux-tu le rajouter au contact? OUI ou NON ?")
+            if add == "OUI":
+                add_contact(c)
+                a = True
+            else:
+                print("Tu peux créer un nouveaux contact: ")
+
